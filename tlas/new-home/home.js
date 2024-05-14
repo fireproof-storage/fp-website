4565af4f7e01b5e1f8abe1dd4c5311bc748e1e68
HelloXiuXiu <xiuxiuweb@gmail.com>
2024-05-14 15:43:51 +0200

    tab styles fix

--- tlas/new-home/home.js
+++ tlas/new-home/home.js
@@ -1,2 +1,16 @@
 'use strict'
 
+// demo app tabs
+const tabs = document.querySelector('.hero-app-tabs')
+
+tabs.onclick = (e) => {
+  e.stopPropagation()
+  const targetTab = e.target.closest('.tab')
+  if (!targetTab) return
+
+  tabs.querySelectorAll('.tab').forEach(tab => {
+    tab.classList.remove('active')
+  })
+  targetTab.classList.add('active')
+}
+
