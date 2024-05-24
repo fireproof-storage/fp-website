881965d82c2f33fbf5c9004cc7e45c53bcb42686
HelloXiuXiu <xiuxiuweb@gmail.com>
2024-05-24 23:08:17 +0200

    logos section wip

--- tlas/new-home/testimonials-carusel.js
+++ tlas/new-home/testimonials-carusel.js
@@ -1,29 +1,44 @@
 'use strict'
 
-// testimonials infinite slider
+// testimonials and logos infinite slider
 // works toghether with css keyframes
 
-const tastimonials = document.querySelector('.testimonials-list')
+const testimonials = document.querySelector('.testimonials-list')
+const logos = document.querySelector('.logos-list')
+
 const duration = 8
-const innerContent = tastimonials.innerHTML
+
+const innerContentTestimonials = testimonials.innerHTML
+const innerContentLogos = logos.innerHTML
 
 function runTestimonialsAnimation() {
   const w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
-  let d = duration
+  let testimonialsDuration = duration
+  let logosDuration = duration
 
-  while (tastimonials.offsetWidth < w * 2) {
-    d += duration
-    tastimonials.innerHTML += innerContent
+  while (testimonials.offsetWidth < w * 2) {
+    testimonialsDuration += duration
+    testimonials.innerHTML += innerContentTestimonials
   }
 
-  tastimonials.style.animationDuration = `${d}s`
-  tastimonials.classList.add('testimonials-animate')
+  while (logos.offsetWidth < w * 2) {
+    logosDuration += duration
+    logos.innerHTML += innerContentLogos
+  }
+
+  logos.style.animationDuration = `${logosDuration}s`
+  testimonials.style.animationDuration = `${testimonialsDuration}s`
+
+  testimonials.classList.add('testimonials-animate')
+  logos.classList.add('logos-animate')
 }
 
 runTestimonialsAnimation()
 
 window.addEventListener('resize', () => {
-  tastimonials.innerHTML = innerContent
-  tastimonials.classList.remove('testimonials-animate')
+  testimonials.innerHTML = innerContentTestimonials
+  logos.innerHTML = innerContentLogos
+  testimonials.classList.remove('testimonials-animate')
+  logos.classList.remove('logos-animate')
   runTestimonialsAnimation()
 })
