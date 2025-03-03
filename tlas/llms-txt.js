23e639bdbb3b596b370386fa25e063e07fa23f43
Zach Rose <zach@windmountainsoftware.com>
2025-03-03 14:51:12 -0500

    Remove fetch off llms-mini.txt

--- tlas/llms-txt.js
+++ tlas/llms-txt.js
@@ -3,19 +3,11 @@
 {
   document.addEventListener('DOMContentLoaded', () => {
     let llmTxt = "";
-    let llmMiniTxt = "";
     const llmTxtUrl = "https://use-fireproof.com/llms.txt";
-    const llmMiniTxtUrl = "https://use-fireproof.com/llms-mini.txt";
-
     const llmTxtDisplay = document.getElementById('llm-txt');
     const llmTxtButton = document.getElementById('copy-llm-txt');
     const buttonText = llmTxtButton.innerText;
     const buttonContent = llmTxtButton.innerHTML;
-
-    const fetchMiniAndDisplay = async () => {
-        await fetchMini();
-        displayMini(llmMiniTxt)
-    }
 
     const fetchMedium = async () => {
       try {
@@ -27,25 +19,14 @@
       }
     }
 
-    const fetchMini = async () => {
-      try {
-        const response = await fetch(llmMiniTxtUrl);
-        llmMiniTxt = await response.text();
-      } catch (error) {
-        console.error('Error fetching mini text:', error.message);
-      }
+    const displayMedium = () => {
+      const formattedText = llmTxt.replace(/\n/g, ' ').replace(/\s+/g, ' ');
+      llmTxtDisplay.innerText = formattedText;
     }
-
-    const displayMini = (miniText) => {
-        const formattedText = miniText.replace(/\n/g, ' ').replace(/\s+/g, ' ');
-        llmTxtDisplay.innerText = formattedText;
-    }
-
 
     const setClipboard = async () => {
       try {
         await navigator.clipboard.writeText(llmTxt);
-        
         llmTxtButton.innerText = "Copied llms.txt to Clipboard!";
         setTimeout(() => {
           llmTxtButton.innerHTML = buttonContent;
@@ -63,8 +44,10 @@
       setClipboard();
     });
 
-    // On page load, fetch mini text and display it
-    fetchMiniAndDisplay();
-    fetchMedium();
+    // On page load, fetch llm.txt and display it
+    (async () => {
+      await fetchMedium();
+      displayMedium();
+    })();
   })
 }
